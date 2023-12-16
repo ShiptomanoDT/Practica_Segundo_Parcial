7bd464ba8507195d39d7127a39d08ce496fedd20
Waldo <shiptomano@gmail.com>
2023-12-15 22:18:53 -0400

    Se agrego CRUD Usuarios
    Se Realizo la primera migracion del la base de datos junto a la actualizacion de la Base de Datos, se añadio un nuevo controlador para la clase Usuario, se agrego la vista Usuario al interfaz configurando el archivo _Layout, se hicieron cambios en el interfaz de la aplicacion web pra traducir algunos elementos al español

--- AquaFreeSystem/AquaFreeSystem/Context/MiContext.cs
+++ AquaFreeSystem/AquaFreeSystem/Context/MiContext.cs
@@ -4,5 +4,9 @@ namespace AquaFreeSystem.Context
 {
     public class MiContext : DbContext
     {
+        public MiContext(DbContextOptions<MiContext> options) : base(options)
+        {
+        }
+        public DbSet<Models.Usuario> Usuarios { get; set; }
     }
 }
--- AquaFreeSystem/AquaFreeSystem/Dtos/RolEnum.cs
+++ AquaFreeSystem/AquaFreeSystem/Dtos/RolEnum.cs
@@ -2,7 +2,7 @@
 {
     public enum RolEnum
     {
-        Gerente,
+        Gerente = 1,
         Empleado
     }
 }
